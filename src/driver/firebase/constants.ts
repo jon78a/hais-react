484bc1e2c7e1a0f3df951b5e23f4682b098c5559
jinhyeok15 <ekfrhd97@gmail.com>
2023-12-04 23:15:27 +0900

    feat(#77): 학과 / 과목군그룹 및 추천 그룹 Repository 개발

--- src/driver/firebase/constants.ts
+++ src/driver/firebase/constants.ts
@@ -13,10 +13,21 @@ export class CollectionName {
 }
 
 export class StorageSource {
-  static GeneralMajor = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/generalMajor.json?alt=media&token=d74e42a9-cace-4c18-ac4e-b1501590ee78";
-  static Major = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/major.json?alt=media&token=a966858e-023f-4986-8756-929688f30b23";
-  static MajorCategoryGroup = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/majorCategoryGroup.json?alt=media&token=2bd327d1-4c0e-4c7c-8336-291ef09fa303";
-  static OptionalSubject = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/optionalSubject.json?alt=media&token=88e3798b-3f7d-4534-9b99-d5655fb89646";
-  static RecommendGroup = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/recommendGroup.json?alt=media&token=a0b53268-3e90-4236-b25b-a0178d289c47";
-  static Univ = "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/univ.json?alt=media&token=216969cb-0f37-4dec-b722-a473221cc5b1";
+  static GeneralMajor =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/generalMajor.json?alt=media&token=d74e42a9-cace-4c18-ac4e-b1501590ee78";
+  static Major =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/major.json?alt=media&token=a966858e-023f-4986-8756-929688f30b23";
+  static MajorCategoryGroup =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/majorCategoryGroup.json?alt=media&token=2bd327d1-4c0e-4c7c-8336-291ef09fa303";
+  static OptionalSubject =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/optionalSubject.json?alt=media&token=88e3798b-3f7d-4534-9b99-d5655fb89646";
+  static RecommendGroup =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/recommendGroup.json?alt=media&token=a0b53268-3e90-4236-b25b-a0178d289c47";
+  static Univ =
+    "https://firebasestorage.googleapis.com/v0/b/hais-72666.appspot.com/o/univ.json?alt=media&token=216969cb-0f37-4dec-b722-a473221cc5b1";
+
+  static filePath = {
+    majorCategoryGroup: "majorCategoryGroup.json",
+    recommendGroup: "recommendGroup.json",
+  };
 }
--- src/driver/firebase/types.ts
+++ src/driver/firebase/types.ts
@@ -5,7 +5,7 @@ export type GeneralMajorType = {
   clsf_name: string;
   name: string;
   description: string;
-}
+};
 
 export type MajorType = {
   id: number;
@@ -19,33 +19,33 @@ export type MajorType = {
   _specification?: string;
   investigation_year: string;
   status: "DELETE" | "ACTIVE";
-}
+};
 
 export type MajorCategoryGroupType = {
   id: number;
   major_id: number;
   general_code: string;
-}
+};
 
 export type OptionalSubjectType = {
   code: string;
   group: string;
-  student_category?: StudentCategoryCode,
+  student_category?: StudentCategoryCode;
   category: string;
   name: string;
   description: string;
   suneung_info: string;
   etc_info: string;
-}
+};
 
 export type RecommendGroupType = {
-  id: number;
+  id: string;
   gnr_mjr_code: string;
   opt_sbj_code: string;
-}
+};
 
 export type UnivType = {
   id: number;
   name: string;
   sido_code: string;
-}
+};
