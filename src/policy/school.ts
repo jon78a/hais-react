1b928b7293cbb8b82334febdb110060f60315c0f
2hyeong <doo.hyeongnim@gmail.com>
2024-02-18 18:04:15 +0900

    chore(admin): add school policies

--- src/policy/school.ts
+++ src/policy/school.ts
@@ -1,4 +1,5 @@
 export type StudentCategoryCode = "A" | "B" | "C" | "D" | "E" | "NONE";
+export type SchoolOperation = "사립학교" | "공립학교";
 
 export const studentCategoryMap = {
   NONE: "해당없음",
@@ -6,3 +7,8 @@ export const studentCategoryMap = {
   B: "자연계",
   C: "예체능",
 };
+
+export const schoolOperationMap = {
+  PRIVATE: "사립학교",
+  PUBLIC: "공립학교",
+} as const;
